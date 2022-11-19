69a923cea5fec99e94ef5976f8134cf83fce4432
unknown <joseph.gakure@thejitu.com>
2022-11-19 13:40:04 +0300

    new..

--- src/componets/products/Product.jsx
+++ src/componets/products/Product.jsx
@@ -2,13 +2,14 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchData } from "../../redux/slice/ProductSlice";
 import Navbar from "../Navbar/Navbar";
+import "./Product.css"
 
 function Product() {
   const dispatch = useDispatch();
 
   const data = useSelector((state) => state.products.products);
 
-  console.log('from firebase',data);
+  console.log("from firebase", data);
 
   useEffect(() => {
     dispatch(fetchData());
@@ -18,23 +19,21 @@ function Product() {
     <div>
       <Navbar />
       <div className="product--data">
-        <h3>test</h3>
-       {data?.map(product =>(
-        <div className="product--card">
-          <div className="imaage">
-            <img src={product.image} alt="image" />
+        {/* <h3>items</h3> */}
+        {data?.map((product) => (
+          <div className="product--card">
+            <div className="imaage">
+              <img src={product.image} alt="ima" />
+            </div>
+            <div className="doc">
+              <h3>{product.title}</h3>
+              <p>{product.body}</p>
+              <p>{product.price}</p>
+              <p>{product.discountRate}</p>
+              <button>add products</button>
+            </div>
           </div>
-        <div>
-          <h3>{product.title}</h3>
-          <p>{product.body}</p>
-          <p>{product.price}</p>
-          <p>{product.discountRate}</p>
-          <button>add products</button>
-        </div>
-          
-          
-        </div>
-       ))}
+        ))}
       </div>
     </div>
   );
